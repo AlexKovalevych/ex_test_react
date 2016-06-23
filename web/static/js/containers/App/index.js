cf04f9c7bc5a4a4f6bc5c2d278844d5538a3b40d
Alex Kovalevych <alexkovalevych@gmail.com>
2016-06-24 01:49:36 +0300

    add frontend classes

---
+++ web/static/js/containers/App/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Link, IndexLink } from 'react-router';
+
+export default class App extends React.Component {
+    render() {
+        return (
+            <div className="container">
+                <header className="header">
+                    <nav role="navigation">
+                        <ul className="nav nav-pills pull-right">
+                            <li><Link className="btn" to="/auth/login">Login</Link></li>
+                            <li><Link className="btn" to="/auth/register">Register</Link></li>
+                        </ul>
+                    </nav>
+                    <IndexLink to="/"><span className="logo"></span></IndexLink>
+                </header>
+                {this.props.children}
+            </div>
+        );
+    }
+}
